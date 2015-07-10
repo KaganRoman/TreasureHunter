6b750efe7224ece3cd7f3b864428d14f2827e5f8
Roman Kagan <roman@rumble.me>
2015-07-10 17:12:02 +0300

    add timer

--- MainPageVM.cs
+++ MainPageVM.cs
@@ -3,6 +3,10 @@ using System.ComponentModel;
 using Xamarin.Forms;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 
 namespace BeaconTest
 {
@@ -18,11 +22,21 @@ namespace BeaconTest
 
 		private IEnumerable<BeaconStatus> _beaconsStatus;
 
+		private string _timer;
+		private CancellationTokenSource _timerCancel;
+
+		private Xamarin.Forms.Command _startCommand;
+		private Xamarin.Forms.Command _stopCommand;
+		private bool _started;
+
 		public MainPageVM ()
 		{
 			_beaconReceiver = DependencyService.Get<IBeaconReceiver> ();
 			_beaconReceiver.BeaconsUpdated += (sender, e) => BeaconsStatus = e;
 			_beaconsStatus = new List<BeaconStatus> ();
+
+			_timer = "";
+
 		}
 
 		public IEnumerable<BeaconStatus> BeaconsStatus 
@@ -30,11 +44,78 @@ namespace BeaconTest
 			get { return _beaconsStatus; }
 			set {
 				_beaconsStatus = value;
-				OnPropertyChange ();
+				OnPropertyChanged ();
 			}
 		}
 
-		private void OnPropertyChange([CallerMemberName] string name = null)
+		public Xamarin.Forms.Command StartCommand
+		{
+			get {
+				return _startCommand = _startCommand ?? new Command (Start, () => !Started);
+			}
+		}
+
+		public Xamarin.Forms.Command StopCommand
+		{
+			get {
+				return _stopCommand = _stopCommand ?? new Command (Stop, () => Started);
+			}
+		}
+
+		public string Timer
+		{
+			get { return _timer; }
+			set {
+				_timer = value;
+				OnPropertyChanged ();
+			}
+		}
+
+		public bool Started
+		{
+			get { return _started; }
+			set {
+				_started = value;
+				StartCommand.ChangeCanExecute ();
+				StopCommand.ChangeCanExecute ();
+				OnPropertyChanged ();
+			}
+		}
+
+		private void Start()
+		{
+			Started = true;
+			if (_timerCancel != null)
+				_timerCancel.Cancel ();
+			_timerCancel = new CancellationTokenSource ();
+			Task.Factory.StartNew (StartTimer);
+		}
+
+		private void Stop()
+		{
+			Started = false;
+			if (_timerCancel != null)
+				_timerCancel.Cancel ();
+			_timerCancel = null;
+		}
+
+		private async Task StartTimer()
+		{
+			try
+			{
+				var cancel = _timerCancel;
+				var st = Stopwatch.StartNew();
+				Timer = "00:00";
+				while (!cancel.IsCancellationRequested) {
+					await Task.Delay (1000, cancel.Token);
+					Timer = string.Format("{0:00}:{1:00}", st.ElapsedMilliseconds/60000, (st.ElapsedMilliseconds/1000)%60000);
+				}
+			}
+			catch {
+			}
+		}
+
+		private void OnPropertyChanged([CallerMemberName] string name = null)
 		{
 			if (PropertyChanged != null) {
 				PropertyChanged(this, new PropertyChangedEventArgs(name));
