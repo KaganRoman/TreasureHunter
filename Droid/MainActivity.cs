8c829a98cdae68dd480e973354ca7f5d7d8388c0
Roman Kagan <roman@rumble.me>
2015-07-14 11:16:18 +0300

    add droid binding

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -8,6 +8,8 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Com.Roximity.Sdk;
+
 namespace BeaconTest.Droid
 {
 	[Activity (Label = "BeaconTest.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -16,6 +18,9 @@ namespace BeaconTest.Droid
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+
+			ROXIMITYEngine.startEngineWithOptions(this.getApplicationContext(), R.drawable.ic_launcher, null, this, null);
+
 
 			global::Xamarin.Forms.Forms.Init (this, bundle);
 
